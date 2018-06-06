09c91da29cff522bb3702d61d064f24bbf38f512
Cloud User <ec2-user@ip-172-31-43-37.us-east-2.compute.internal>
2018-06-06 15:25:38 +0000

    palli.c added

---
+++ palli.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void palli()
+{
+    int n, reversedInteger = 0, remainder, originalInteger;
+
+    printf("Enter an integer to check whether given number is pallindrome or not: ");
+    scanf("%d", &n);
+
+    originalInteger = n;
+
+    // reversed integer is stored in variable 
+    while( n!=0 )
+    {
+        remainder = n%10;
+        reversedInteger = reversedInteger*10 + remainder;
+        n /= 10;
+    }
+
+    // palindrome if orignalInteger and reversedInteger are equal
+    if (originalInteger == reversedInteger)
+        printf("%d is a palindrome.", originalInteger);
+    else
+        printf("%d is not a palindrome.", originalInteger);
+    
+    }
